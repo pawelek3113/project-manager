9b5c928c727b76610ee768bf7e7270238161be9d
Paweł Komendera <pawel.kamil.komendera@gmail.com>
2024-07-26 14:21:00 +0200

    feat: implement App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,39 @@
+import { useState } from "react";
+import Navbar from "./components/Navbar";
+import NewProject from "./components/NewProject";
+import NoProjectSelected from "./components/NoProjectSelected";
+import Sidebar from "./components/Sidebar";
 function App() {
-  return (
-    <>
-      <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1>
-    </>
-  );
+	const [projectsState, setProjectsState] = useState({
+		selectedProjectId: undefined,
+		projects: [],
+	});
+
+	function handleAddProject() {
+		setProjectsState((prevProjectsState) => {
+			return { ...prevProjectsState, selectedProjectId: null };
+		});
+	}
+	function handleCancel() {
+		setProjectsState((prevProjectsState) => {
+			return { ...prevProjectsState, selectedProjectId: undefined };
+		});
+	}
+
+	const content =
+		projectsState.selectedProjectId === null ? (
+			<NewProject onCancel={handleCancel} />
+		) : (
+			<NoProjectSelected onAddingProject={handleAddProject} />
+		);
+
+	return (
+		<main className="h-screen w-full flex flex-col md:flex-row gap-4 md:gap-10 bg-slate-800 text-white">
+			<Navbar onAddingProject={handleAddProject} />
+			<Sidebar onAddingProject={handleAddProject} />
+			<div className="flex flex-col gap-2">{content}</div>
+		</main>
+	);
 }
 
 export default App;
