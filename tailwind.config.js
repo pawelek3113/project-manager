0f0a74bc751a89b67d6a1787d4a7e8b7a152ef59
Paweł Komendera <pawel.kamil.komendera@gmail.com>
2024-07-26 14:11:09 +0200

    chore: update Tailwind CSS configuration for image background size

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+	theme: {
+		extend: {
+			backgroundSize: {
+				"size-200": "200% 200%",
+			},
+		},
+	},
+	plugins: ["prettier-plugin-tailwindcss"],
+};
