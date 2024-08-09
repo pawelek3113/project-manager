37f037fc91092af062a4eecca52fe44581f51c9e
Paweł Komendera <pawel.kamil.komendera@gmail.com>
2024-08-09 14:11:26 +0200

    refactor: Update due date display in Project component

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,12 +1,16 @@
 export default function Project({ project }) {
-	console.log(project);
+	const dueDate = new Date(project.dueDate);
 	return (
 		<div className="px-4 py-6 flex flex-col gap-8">
 			<div className="gap-3 flex flex-col">
 				<h1 className="text-5xl tracking-tight font-bold">
 					{project.title}
 				</h1>
-				<h3 className="text-sm font-bold">DUE: {project.dueDate}</h3>
+				<div>
+					<h3 className="text-sm font-bold">
+						DUE: {dueDate.toLocaleDateString()}
+					</h3>
+				</div>
 			</div>
 			<p>{project.description}</p>
 		</div>
