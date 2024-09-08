10a73fb81cd49836b3c43b1ad6f2dff4b3032403
Paweł Komendera <pawel.kamil.komendera@gmail.com>
2024-09-08 12:33:18 +0200

    refactor: Update project name in package.json and improve Input component
    The project name in `package.json` has been updated from "refs-demo-project" to "project-manager". Additionally, the `Input` component has been improved to support different variants and custom class names.

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,19 +1,32 @@
 import { forwardRef } from "react";
 
-const Input = forwardRef(function Input({ label, textarea, ...props }, ref) {
-	const inputClasses =
-		"border rounded-lg focus:outline-none focus:outline-green-400 text-black";
+const Input = forwardRef(function Input(
+	{ label, textarea, variant = "default", className, ...props },
+	ref
+) {
+	const VARIANTS = {
+		default:
+			"border rounded-lg focus:outline-none focus:outline-green-400 text-black p-1",
+		ghost: "bg-transparent outline-none p-1",
+	};
+
+	let chosenVariant = `${VARIANTS[variant]} ${className && className}`;
+
 	return (
 		<p className="flex flex-col gap-1">
-			<label className="uppercase text-sm font-bold text-gray-300">
-				{label}
-			</label>
+			{label ? (
+				<label className="uppercase text-sm font-bold text-gray-300">
+					{label}
+				</label>
+			) : (
+				""
+			)}
 			{!textarea ? (
-				<input ref={ref} className={inputClasses} {...props} />
+				<input ref={ref} className={chosenVariant} {...props} />
 			) : (
 				<textarea
 					ref={ref}
-					className={inputClasses}
+					className={chosenVariant}
 					{...props}
 				></textarea>
 			)}
