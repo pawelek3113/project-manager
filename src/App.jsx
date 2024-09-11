4d84af95d7d4a6c26c241f95d1942e1c88346e88
Paweł Komendera <pawel.kamil.komendera@gmail.com>
2024-09-11 17:43:01 +0200

    refactor: Update selected project ID
    The selected project ID is updated to the provided project ID

--- src/App.jsx
+++ src/App.jsx
@@ -33,7 +33,7 @@ function App() {
 
       return {
         ...prevProjectsState,
-        selectedProjectId: undefined,
+        selectedProjectId: projectId,
         projects: [...prevProjectsState.projects, newProject],
       };
     });
